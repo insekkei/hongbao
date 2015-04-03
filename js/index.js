3fa82928c95bc33e718c1ee1334d6f39af77bc49
insekkei <insekkei@yahoo.com>
2015-04-03 17:35:08 +0800

    ...

--- js/index.js
+++ js/index.js
@@ -1,14 +1,24 @@
 $(function(){
-	var num=0;
+	var num=0,
+	     k = 200;
+	     html = '<a href=""></a>';
+	for (k=0;k<200;k++){
+		$('#hongbao').append(html);
+		$('#hongbao a:nth-child('+k+')').css({
+			'left':k+'%',
+			'-webkit-animation':'translation 4s '+k+'s  linear'
+		});
+	}
 	$('.get-hb').click(function(e){
 		$('#hongbao').fadeIn(0);
 		e.preventDefault();
 	});
-	$('#hongbao a').click(function(e){
-		//alert(typeof Number($('#num').html()));
+	$(document).on('click','#hongbao a',function(e){
+		$(this).addClass('visited');
 		num+=1;
 		$('#num').html(num);
 		e.preventDefault();
 	});
+
 
 });
