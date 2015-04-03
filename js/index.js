db225916124905f077df28beb1551907f736b909
insekkei <insekkei@yahoo.com>
2015-04-03 21:48:49 +0800

    ...

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
 	var num=0,
 	     k = 200;
 	     html = '<a href=""></a>';
@@ -21,4 +21,37 @@ $(function(){
 	});
 
 
-});
+});*/
+
+        function start() {
+        	$('#host').fadeIn();
+            randomOneRed(0, 50);
+        }
+
+        function randomOneRed(current, max) {
+            if (current >= max) {
+                return;
+            } else {
+                createRedBag();
+                setTimeout(randomOneRed, 500, current + 1, max);
+            }
+        }
+        function createRedBag() {
+            var width = Math.random() * 60 + 30;
+            var height = width * 1.5;
+            var pleft = Math.random() * ($(window).width() - width);
+            var theight = $(window).height();
+            var id = 'bag_' + Math.floor(Math.random() * 10000000);
+            $('<div></div>')
+                .attr("id", id)
+                .width(width)
+                .height(height)
+                .css(
+                    {
+                        "background-color": "transparent",
+                        "position": "absolute",
+                        "left": pleft,
+                        "top": 0
+                    })
+                .appendTo("#host").animate({ top: theight }, 3000, 'linear', function () { $("#" + id).hide(); });
+        }
