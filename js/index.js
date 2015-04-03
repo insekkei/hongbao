fb4495f89957d4ce5388297b07bb4c0bd00b5d27
insekkei <insekkei@yahoo.com>
2015-04-03 17:15:41 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 $(function(){
+	var num=0;
+	$('.get-hb').click(function(e){
+		$('#hongbao').fadeIn(0);
+		e.preventDefault();
+	});
 	$('#hongbao a').click(function(e){
-		parseInt($('#num').html())+=1;
+		//alert(typeof Number($('#num').html()));
+		num+=1;
+		$('#num').html(num);
 		e.preventDefault();
 	});
 
